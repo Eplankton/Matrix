564779931f4a0c4a1bc00172fc6f6e63e5258e1e
Eplankton <86543401+Eplankton@users.noreply.github.com>
2021-07-24 12:56:08 +0800

    Update and rename matrix.c to main.c

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "matrixFunction.h"
+
+int main()
+{
+    int key = 1; // User Interface
+
+    for (; key > 0;)
+    {
+        printf("\n\n");
+        printf("                          #############################\n");
+        printf("                                   @ Matrix            \n");
+        printf("                                                       \n");
+        printf("                                     A + B         :1  \n");
+        printf("                                     A - B         :2  \n");
+        printf("                                     a * M         :3  \n");
+        printf("                                     A x B         :4  \n");
+        printf("                                     Det(A)        :5  \n");
+        printf("                                     A ^ T         :6  \n");
+        printf("                                     Exit          :0  \n");
+        printf("                                                       \n");
+        printf("                          #############################\n");
+        printf("                           Please enter : ");
+        scanf("%d", &key);
+
+        switch (key)
+        {
+        case 1:
+            matrixAddition();
+            break;
+        case 2:
+            matrixSubtraction();
+            break;
+        case 3:
+            scalarMultiplication();
+            break;
+        case 4:
+            matrixMultiplication();
+            break;
+        case 6:
+            matrixTransposition();
+            break;
+        case 0:
+            break;
+        }
+    }
+
+    return 0;
+}
