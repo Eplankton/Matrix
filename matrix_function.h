2f7cb9ecf6f8ba593b563fb323b5b43addd29df7
Eplankton <86543401+Eplankton@users.noreply.github.com>
2021-07-27 19:03:39 +0800

    Add determinant

--- matrix_function.h
+++ matrix_function.h
@@ -3,20 +3,22 @@ void matrixAddition();
 void matrixSubtraction();
 void scalarMultiplication();
 void matrixMultiplication();
+void matrixDeterminant();
 void matrixTransposition(); //Function declaration
 
-int row = 0, column = 0, temp = 0;
+int row = 0, column = 0;
+int prov = 0, temp = 0; //Use another value to transport 'row'
 int i = 0, j = 0;
 
 void matrixInput(float *r) //Function to input matrix
 {
-    char matrixName[20];
+    char matrixName[30];
     getchar();
-    printf("\n\nSet the matrix name as :  ");
+    printf("\n\nSet the matrix name (No space) : ");
     scanf("%s", &matrixName);
     printf("\n");
 
-    printf("\nYour matrix :  %s < %d, %d > \n\n", matrixName, row, column);
+    printf("\nYour matrix : %s < %d, %d > \n\n", matrixName, row, column);
 
     float a[row][column];
     int m = 0, n = 0;
@@ -75,15 +77,15 @@ void matrixInput(float *r) //Function to input matrix
     }
 }
 
-void matrixAddition() //Function to add two matrix together .
+void matrixAddition() //Function to add two matrix together
 {
-    printf("\nMode:1 -> Addition \n\n");
+    printf("\nMode:1 -> Addition\n\n");
     printf("\nPlease set the row of the first matrix = ");
     scanf("%d", &row);
 
     if (row < 1)
     {
-        printf("\nError! Please set the row of the first matrix again= ");
+        printf("\nError! Please set the row of the first matrix again = ");
         scanf("%d", &row);
     }
 
@@ -150,7 +152,7 @@ void matrixAddition() //Function to add two matrix together .
 
 void matrixSubtraction() //Similar to matrixAddition()
 {
-    printf("\nMode:2 -> Subtraction \n\n");
+    printf("\nMode:2 -> Subtraction\n\n");
     printf("\nPlease set the row of the first matrix = ");
     scanf("%d", &row);
 
@@ -222,15 +224,15 @@ void matrixSubtraction() //Similar to matrixAddition()
     }
 }
 
-void scalarMultiplication() //Multiply a scalar on a matrix .
+void scalarMultiplication() //Multiply a scalar on a matrix
 {
-    printf("\nMode:3 -> Scalar Multiplication \n\n");
+    printf("\nMode:3 -> Scalar Multiplication\n\n");
     printf("\nPlease set the row of the matrix = ");
     scanf("%d", &row);
 
     if (row < 1)
     {
-        printf("\nError! Please set the row of the matrix again= ");
+        printf("\nError! Please set the row of the matrix again = ");
         scanf("%d", &row);
     }
 
@@ -276,15 +278,15 @@ void scalarMultiplication() //Multiply a scalar on a matrix .
     }
 }
 
-void matrixMultiplication() //Multiply two matrix together .
+void matrixMultiplication() //Multiply two matrix together
 {
-    printf("\nMode:4 -> Multiplication \n\n");
+    printf("\nMode:4 -> Multiplication\n\n");
     printf("\nPlease set the row of the first matrix = ");
     scanf("%d", &row);
 
     if (row < 1)
     {
-        printf("\nError! Please set the row of the first matrix again= ");
+        printf("\nError! Please set the row of the first matrix again = ");
         scanf("%d", &row);
     }
 
@@ -321,7 +323,6 @@ void matrixMultiplication() //Multiply two matrix together .
     float result[row][column];
     float *q = &latter[0][0];
 
-    int prov = 0; //Use another value to transport 'row' .
     prov = row;
     row = temp;
 
@@ -356,15 +357,48 @@ void matrixMultiplication() //Multiply two matrix together .
     }
 }
 
+void matrixDeterminant()//Determinant
+{
+    printf("\nMode:5 -> matrixDeterminant\n\n");
+    printf("\nPlease set the dimension of the determinant = ");
+    scanf("%d", &row);
+
+    if (row < 2 || row > 3)
+    {
+        printf("\nError! Please set the dimension of the determinant again = ");
+        scanf("%d", &row);
+    }
+
+    column = row;
+
+    float former[row][column];
+    float *p = &former[0][0];
+    matrixInput(p);
+
+    float result = 0;
+
+    if (row == 2)
+    {
+        result = former[0][0] * former[1][1] - former[1][0] * former[0][1];
+    }
+    else
+    {
+        result = former[0][0] * former[1][1] * former[2][2] + former[0][2] * former[1][0] * former[2][1] + former[0][1] * former[1][2] * former[2][0] -
+                 former[0][0] * former[1][2] * former[2][1] - former[0][2] * former[1][1] * former[2][0] - former[0][1] * former[1][0] * former[2][2];
+    }
+
+    printf("\nThe result of 'Det(A)' is : %g\n\n", result);
+}
+
 void matrixTransposition() //Transposition of matrix
 {
-    printf("\nMode:6 -> matrixTransposition \n\n");
+    printf("\nMode:6 -> matrixTransposition\n\n");
     printf("\nPlease set the row of the matrix = ");
     scanf("%d", &row);
 
     if (row < 1)
     {
-        printf("\nError! Please set the row of the matrix again= ");
+        printf("\nError! Please set the row of the matrix again = ");
         scanf("%d", &row);
     }
 
